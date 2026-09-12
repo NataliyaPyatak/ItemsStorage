a3500e8aa7540f1b1b1364bfc9fa526790a031a4
npyatak <npyatak@naumen.ru>
2026-09-12 13:24:02 +0500

    Initial commit: Spring Boot project skeleton

---
+++ src/main/java/ru/npyatak/itemsStorage/ItemsStorageApplication.java
@@ -0,0 +1,13 @@
+package ru.npyatak.itemsStorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemsStorageApplication.class, args);
+	}
+
+}
